96490572c97dd33f96bc8d81d5a4d9aee1c0bfbc
genxhero <genxhero@users.noreply.github.com>
2020-12-07 17:47:12 -0800

    photocard

--- client/src/PhotoCard.jsx
+++ client/src/PhotoCard.jsx
@@ -1,3 +1,12 @@
 /**
- * Display one photo
+ * Display one photo, small format, for gallery pages and such.
  */
+
+ import React from 'react';
+
+ const PhotoCard = props => {
+
+ }
+
+
+ export default PhotoCard;
