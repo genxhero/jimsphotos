8e0c2cdbd4007b90f33c8b591324fd1154f547ce
genxhero <genxhero@users.noreply.github.com>
2020-12-07 23:55:29 -0800

    basic gallery

--- client/src/PhotoGallery.jsx
+++ client/src/PhotoGallery.jsx
@@ -1,3 +1,17 @@
 /**
  * A photo gallery takes a category prop which determines which photos shall be displayed.
  */
+
+import React from 'react';
+import PhotoCard from './PhotoCard';
+
+ const photoGallery = props => {
+     const photos = {props};
+     return (
+        <div>
+            {photos.map(photo => <PhotoCard imageUrl={photo.url}/>)}
+        </div>
+     )
+ }
+
+ export default photoGallery;
